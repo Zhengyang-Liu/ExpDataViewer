31c9b655b8e8d190b93f0f8d4e1da06c24158c1b
Zhengyang Liu <liuzhengyang183@gmail.com>
2019-07-17 19:35:10 +0800

    Add project files.

---
+++ ExperimentsDataViewer/Models/ExpData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace ExperimentsDataViewer.Models
+{
+    public class ExpData
+    {
+        public string ExpID { get; set; }
+        public DateTime CollectedTime { get; set; }
+        public double AccelerationValue { get; set; }
+    }
+}
