690a7809e29346054dba6a57d1d850c29876210c
lcheng <lchenggw@outlook.com>
2019-07-27 05:32:48 -0700

    Set default start page to be ExpInfo

--- ExperimentsDataViewer/App_Start/RouteConfig.cs
+++ ExperimentsDataViewer/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ExperimentsDataViewer
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "ExpInfo", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
